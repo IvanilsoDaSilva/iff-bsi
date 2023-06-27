f65f9aaf68b92c7bf9d0e9b3f073701b3d7db04a
IvanilsoDaSilva <ivanilsodasilva300@gmail.com>
2023-06-26 22:46:56 -0300

    inicio da implementacao de listas sequenciais

--- src/TP/excercicio03/Main.java
+++ src/TP/excercicio03/Main.java
@@ -5,10 +5,23 @@ import TP.excercicio03.entities.StackEntity_Sequencial;
 
 public class Main {
 	public static void main(String[] args) {
+		// Formas de instanciar a pilha encadeada
 //		StackEntity_Encadeada<String> pilha = new StackEntity_Encadeada<String>("primeiro");
-		StackEntity_Encadeada<String> pilha = new StackEntity_Sequencial<String>("primeiro");
+//		pilha.push("segundo");
 		
+//		StackEntity_Encadeada<String> pilha = new StackEntity_Encadeada<String>();
+//		pilha.push("primeiro");
+//		pilha.push("segundo");
+		
+		// Formas de instanciar a pilha sequencial
+//		StackEntity_Sequencial<String> pilha = new StackEntity_Sequencial<String>(new String[] {"primeiro"});
+		
+//		StackEntity_Sequencial<String> pilha = new StackEntity_Sequencial<String>("primeiro", 10);
+//		pilha.push("segundo");
+		
+		StackEntity_Sequencial<String> pilha = new StackEntity_Sequencial<String>(new String[] {"primeiro"}, 10);
 		pilha.push("segundo");
+		
 		
 		System.out.println("Para string: "+pilha.toString());
 		System.out.println("É vazía? "+pilha.isEmpity());
--- src/TP/excercicio03/entities/StackEntity_Sequencial.java
+++ src/TP/excercicio03/entities/StackEntity_Sequencial.java
@@ -1,21 +1,43 @@
 package TP.excercicio03.entities;
 
-import TP.excercicio02.entities.NoEntity;
 import TP.excercicio03.interfaces.StackInterface;
 
 public class StackEntity_Sequencial<T> implements StackInterface<T> {
 	// Fields
-	private int topo = -1;
+	private int first = -1;
 	private Object[] arr; 
+	private int length = 0;
 	
 	// Methods - Construct
 	public StackEntity_Sequencial(int length) {
 		this.arr = new Object[length];
+		this.length = length;
+		first = length-1;
 	}
 	
-//	public StackEntity_Sequencial(int length, T data) {
-//		this.first = new NoEntity<T>(data, null);
-//	}
+	public StackEntity_Sequencial(T data, int length) {
+		this.arr = new Object[length];
+		this.arr[0] = (T) data;
+		this.length = length;
+		first = 0;
+	}
+	
+	public StackEntity_Sequencial(Object[] arr) {
+		this.arr = arr;
+		this.length = arr.length;
+		first = arr.length-1;
+	}
+	
+	public StackEntity_Sequencial(Object[] arr, int length) {
+		this.arr = new Object[length];
+		
+		for(int i=0; i<arr.length;i++) {
+			this.arr[i] = arr[i]; 
+		}
+		
+		this.length = length;
+		first = arr.length-1;
+	}
 	
 	// Methods - Others
 	@Override
@@ -25,10 +47,12 @@ public class StackEntity_Sequencial<T> implements StackInterface<T> {
 	
 	@Override
 	public T pop() {
-		if (this.first != null) {
-			NoEntity<T> aux = this.first;
-			this.first = this.first.getNext();
-			return aux.getData();
+		if (this.first != -1) {
+			T aux = (T) this.arr[first];
+			this.arr[first] = null;
+			this.first--;
+			return aux;
+			
 		} else {
 			return null;
 		}
@@ -36,15 +60,15 @@ public class StackEntity_Sequencial<T> implements StackInterface<T> {
 	
 	@Override
 	public T getTop() {
-		if (this.first != null) {
-			return first.getData();
+		if (this.first != -1) {
+			return (T) this.arr[first];
 		} else {
 			return null;
 		}
 	}
 	
 	public boolean isEmpity() {
-		if (this.first == null) {
+		if (this.first == -1) {
 			return true;
 		} else {
 			return false;
@@ -53,19 +77,20 @@ public class StackEntity_Sequencial<T> implements StackInterface<T> {
 
 	@Override
 	public void push(T data) {
-		NoEntity<T> aux = this.first;
-		this.first = new NoEntity<T>(data, aux);
+		if (this.first != this.length-1) {
+			arr[this.first+1] = data;
+			this.first++;
+		} else {
+			
+		}
 	}
 	
 	@Override
 	public String toString() {
 		String string = "[";
-		NoEntity<T> no = this.first;
 		
-		while (no != null) {
-			string = string+no.getData()+",";
-			
-			no = no.getNext();
+		for (int i=0; i<this.length;i++) {
+			string = string+this.arr[i]+",";
 		}
 		
 		return string+"]";
